85a633076e9494172f0c1b2336699976daa6db82
Brodie Davis <davisb@ainfosec.com>
2018-08-17 00:11:45 -0400

    input element is now autmagically wrapped; now truly independent, no more id's! :D

--- querybar.js
+++ querybar.js
@@ -9,10 +9,8 @@
   }
 
   $.fn.QueryBar = function (options) {
-    console.log('init query bar', this)
+    console.log('init query bar', this);
 
-
-    //todo support multiple elements with .each()
     /*
      * We can always call .each() on a jquery object, even when only one exists
      * Return this so that we can maintain chain-ability
@@ -25,13 +23,11 @@
       // Self represents QueryBar object here
       let self = {};
       self.inputHandler = function () {
-        let $qContainer = $('._qb-container');
-        let $tokenContainer = $('#token-container');
-        // console.log([ele])
-
+        // Remove last token with backspace
+        // todo handle when a value is removed but a key isn't, then a value is readded
         if (event.key === "Backspace" && ele.value.length === 0) {
           // Backspace was pressed with no value, remove the last two items
-          let $lastToken = $tokenContainer.find('._qb-token').last();
+          let $lastToken = self.$tokenContainer.find('._qb-token').last();
           $(ele).val($lastToken.text());
           $lastToken.remove();
         }
@@ -58,18 +54,45 @@
         }
 
         // Add Key:Value pair to query bar
-        $('#token-container').append(generateTokenKey(parts[0]));
-        $('#token-container').append(generateTokenValue(parts[1]));
+        self.$tokenContainer.append(generateTokenKey(parts[0]));
+        self.$tokenContainer.append(generateTokenValue(parts[1]));
 
         // Reset value
         ele.value = "";
 
         // Scroll all the way to the end of the query bar after appending badges
-        $qContainer.scrollLeft($qContainer.width())
+        // self.$container.scrollLeft(self.$container.width()) todo fix this once we can track the container again
+      };
+      self.init = function() {
+        /*
+         * Wrap Text Box in proper wrappers - this is the goal:
+        <div class="_qb-container">
+          <div id="token-container" class="_qb-token-container"></div>
+          <span class="_qb-input-container">
+              <input id="example" type="text" class="_qb-input">
+          </span>
+        </div>
+         */
+        $ele.addClass('_qb-input');
+        // todo get inputContainer and tokenContainer
+        // self.$inputContainer = $ele.wrap('<div class="_qb-input-container"></div>');
+        // self.$container = self.$inputContainer.wrap('<div class="_qb-container"></div>');
+        // self.$tokenContainer = self.$inputContainer.before('<div class="_qb-token-container"></div>');
+
+        // Hacky, but wrap the primary container first...
+        $ele.wrap('<div class="_qb-container"></div>');
+        // Then insert the token container...
+        self.$tokenContainer = $('<div class="_qb-token-container"></div>').insertBefore($ele);
+        // Then wrap the input element in the input wrapper
+        $ele.wrap('<div class="_qb-input-container"></div>');
+
+        // Add key handler to element
+        $ele.on('keydown', self.inputHandler)
       };
 
-      // Add key handler to element
-      $ele.on('keydown', self.inputHandler)
+      self.init();
+
+      return self;
     });
   };
 
