2c0e25eeb4b5b8d6328d26da439dc4216caf758a
Brodie Davis <davisb@ainfosec.com>
2019-01-16 22:02:39 -0500

    code cleanup; fix not scrolling issue; capture all containers

--- querybar.js
+++ querybar.js
@@ -22,9 +22,6 @@
      */
     for (let ele of this) {
       // Do something to each element here.
-      // todo check if element has already been estantiated, and just return existing object
-      // This can be done by storing the QueryBar object (self below)
-      // on the element itself with $ele.data('QueryBar');
       // todo options to update query bar with params (searchParam = "keyName" || false)
       // let ele = this;
       let $ele = $(ele);
@@ -45,7 +42,6 @@
 
       ret.push(self.public);
     }
-    // });
 
     // Return list of all instantiated objects if we received a list of JQuery objects,
     // Otherwise return the single object.
@@ -55,20 +51,21 @@
   // Class definition - Of course, can't use classes before ES6 🙃🙄
   function QueryBar(ele) {
     let self = this;
-    let $ele = $(ele);
+    self.ele = ele;
+    self.$ele = $(ele);
 
     self.constructor = function() {
-      // Self.public is the Public API to expose (on returned object from
+      // Self.public is the Public API to expose
       self.public = {
-        params: {},  // Search parameters by Key:Value
-        _self: self  // Expose self for advance users and debugging
+        params: {},  // Search parameters by Key:Value or Key:[Value1, Value2]
+        _self: self  // Expose self for advanced users and debugging
       };
 
       // Wrap element in surrounding elements
       self._wrapElement();
 
       // Add key handler to element
-      $ele.on('keydown', inputHandler)
+      self.$ele.on('keydown', inputHandler)
     };
 
     self._wrapElement = function () {
@@ -83,29 +80,35 @@
        */
 
       // Add CSS class to element
-      $ele.addClass('_qb-input');
+      self.$ele.addClass('_qb-input');
 
       // todo get inputContainer and tokenContainer
       // self.$inputContainer = $ele.wrap('<div class="_qb-input-container"></div>');
       // self.$container = self.$inputContainer.wrap('<div class="_qb-container"></div>');
-      // self.$tokenContainer = self.$inputContainer.before('<div class="_qb-token-container"></div>');
 
-      // Hacky, but wrap the primary container first...
-      $ele.wrap('<div class="_qb-container"></div>');
-      // Then insert the token container...
-      self.$tokenContainer = $('<div class="_qb-token-container"></div>').insertBefore($ele);
-      // Then wrap the input element in the input wrapper
-      $ele.wrap('<div class="_qb-input-container"></div>');
+      console.log('wrapping ele')
+      console.log(self)
+
+      // Wrap the element with the primary container first...
+      self.$ele.wrap('<div class="_qb-container"></div>');
+        // Then insert the token container alongside the input element...
+      self.$tokenContainer = $('<div class="_qb-token-container"></div>').insertBefore(self.$ele);
+      // Then wrap the input element with the input wrapper
+      self.$ele.wrap('<div class="_qb-input-container"></div>');
+
+      // Retrieve container and inputContainer
+      self.$container = self.$ele.closest('._qb-container');
+      self.$inputContainer = self.$ele.closest('._qb-input-container');
     };
 
     function inputHandler(e) {
       // Remove last token with backspace
       // todo handle when the input box has styling applied to it
       // todo handle when a value is removed but a key isn't, then a value is readded
-      if (e.key === "Backspace" && ele.value.length === 0) {
+      if (e.key === "Backspace" && self.$ele.val().length === 0) {
         // Backspace was pressed with no value, remove the last two items
         let $lastToken = self.$tokenContainer.find('._qb-token').last();
-        $ele.val($lastToken.text());
+        self.$ele.val($lastToken.text());
         $lastToken.remove();
       }
 
@@ -114,7 +117,8 @@
         return;
 
       // If key was enter, get value
-      let val = ele.value;
+      let val = self.$ele.val()
+      console.log(val)
 
       // If no value present, assume user intended to perform query
       if (!val) {
@@ -136,10 +140,10 @@
       self.public.params[parts[0]] = parts[1];
 
       // Reset value
-      ele.value = "";
+      self.$ele.val("");
 
       // Scroll all the way to the end of the query bar after appending badges
-      // self.$container.scrollLeft(self.$container.width()) todo fix this once we can track the container again
+      self.$container.scrollLeft(self.$container.width());
     }
 
     self.constructor();
